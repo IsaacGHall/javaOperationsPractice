03b60bde58b2b0c149e3a510cd0baf8044e86bb8
ighall96 <123019909+IsaacGHall@users.noreply.github.com>
2023-05-15 15:31:32 -0400

    update NumberOperationsPractice.java
    added prime check functionality.

--- projects/java/numberOperationsPractice.java
+++ projects/java/numberOperationsPractice.java
@@ -11,10 +11,26 @@ public class numberOperationsPractice {
 
         }
     }
+
+public static boolean isPrime(int number){
+    if (number <= 1){
+        return false;
+    } else {
+        for (int i = 2; i <= Math.sqrt(number); i++){ //check factors if they are divisible.
+            if (number % 1 == 0) { //if there is no remainder to the number, it is divisible. 
+                return false;
+            }
+        }
+        return true;
+    }
+}
     public static void main(String[] args) {
         Random rand = new Random(); //rng
-        int factorialNum = rand.nextInt(20)+1; //bounds set at 20, this is the Long.MAX_VALUE's limit. 
+        int factorialNum = rand.nextInt(19)+1; //bounds set at 19, this is the Long.MAX_VALUE's limit.
+        int primeNum = rand.nextInt(2147483646)+1; //maximum int value bounds + 1
         System.out.println("Factorial of " + factorialNum + " is: " + calculateFactorial(factorialNum)); 
+        System.out.println("Prime number check: " + primeNum + " is " + (isPrime(primeNum) ? "prime" : "not prime")); //ternary operator allows for shorthand, basically checking if true or not, and then displaying the correct string.
+
     }
 
 
